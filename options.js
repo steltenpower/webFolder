2d740b2a58c770df78297640c34d775c3931bc9b
Ruud Steltenpool <github@steltenpower.com>
2016-05-25 13:48:53 +0200

    fixed MIMEtypes and importing them
    and now of course they need to be used

--- options.js
+++ options.js
@@ -1,7 +1,6 @@
 function saveOptions(){
-	var temp=document.getElementById("domains").value.trim().split("\n")
-	console.log(temp)
-	localStorage.domains=temp
+	localStorage.domains=document.getElementById("domains").value.trim().split("\n")
+	localStorage.MIMEtypes=document.getElementById("MIMEtypes").toLowerCase().value.trim().split("\n")
 }
 
 document.getElementById('save').addEventListener('click',saveOptions)
